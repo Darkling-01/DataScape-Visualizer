987ab59449d00356ab980fbe2631c16e108ed459
Alejandro Barragan <Alejandro Barragan@DESKTOP-C3E9U7N>
2024-04-12 23:17:33 -0700

    connection between java and html to send response

--- src/main/java/com/app/LoginServlet.java
+++ src/main/java/com/app/LoginServlet.java
@@ -1,11 +1,15 @@
 package com.app;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 /**
  * Servlet implementation class LoginServlet
@@ -26,7 +30,18 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//add code here
+		
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String userId = request.getParameter("userId");
+		String password = request.getParameter("password");
+		
+		out.print("UserId: " + userId);
+		out.print("Password: " + password);
+		
+		
 	}
 
 	/**
